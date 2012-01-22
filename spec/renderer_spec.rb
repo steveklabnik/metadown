1474100bf65887dcafe5788ff0a48ae6de605e8a
Steve Klabnik <steve@steveklabnik.com>
2012-01-22 18:24:10 -0500

    Made renderer properly render markdown

--- spec/renderer_spec.rb
+++ spec/renderer_spec.rb
@@ -6,6 +6,10 @@ describe Metadown::Renderer do
     subject.should be_kind_of(Redcarpet::Render::HTML)
   end
 
+  it "renders markdown" do
+   r = Redcarpet::Markdown.new(subject)
+   r.render("hello world").should eql("<p>hello world</p>\n")
+  end
+
   it "extracts YAML"
-  it "renders markdown" 
 end
